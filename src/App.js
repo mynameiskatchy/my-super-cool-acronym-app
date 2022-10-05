0b213460375d5b84517be6d0f1fa6203c882cfe8
Karchie Trinh <karchietrinh@gmail.com>
2022-10-05 12:58:16 -0400

    Initial commit for acronym app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useMemo } from "react";
 
-function App() {
+const App = ({ orders }) => {
+  const [acronymName, setacronymName] = useState("");
+  const [description, setDescription] = useState("");
+  const [acronyms, setAcronyms] = useState([
+    { name: "EOI", description: "Evidence of Insurability" },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setacronymName(e.target.value)}
+        value={acronymName}
+      />
+      <input
+        type="text"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      />
+      <button
+        onClick={() => {
+          const newAcronym = { name: acronymName, description };
+          const newAcronyms = [...acronyms];
+          newAcronyms.push(newAcronym);
+          setAcronyms(newAcronyms);
+        }}
+      >
+        Add Acronym
+      </button>
+
+      <h2>List of Acronyms</h2>
+
+      {acronyms.map((acronym, index) => (
+        <div key={index}>
+          <button
+            onClick={() => {
+              let newAcronyms = [...acronyms];
+              newAcronyms.splice(index, 1);
+              setAcronyms(newAcronyms);
+            }}
+          >
+            Delete
+          </button>
+          <strong>{acronym.name}</strong> <span>- {acronym.description}</span>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
